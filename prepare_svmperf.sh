bcb84324570a526e84d7c72c1420ce680def500a
Alex Moreo <alejandro.moreo@isti.cnr.it>
2020-12-15 15:28:20 +0100

    cleaning

--- prepare_svmperf.sh
+++ prepare_svmperf.sh
@@ -8,11 +8,6 @@ mkdir ./svm_perf
 tar xvzf $FILE -C ./svm_perf
 rm $FILE
 
-#para crear el patch [para mi]
-#diff -Naur svm_perf svm_perf_quantification > svm-perf-quantification-ext.patch
-
-#para crear la modificacion
-#cp svm_perf svm_perf_quantification -r [ESTO NO HACE FALTA]
 patch -s -p0 < svm-perf-quantification-ext.patch
 mv svm_perf svm_perf_quantification
 cd svm_perf_quantification
