daeb3cdd884a0c9952830ac28042bccde29269e0
Alex Moreo <alejandro.moreo@isti.cnr.it>
2021-03-10 11:40:28 +0100

    cleaning master

--- NewMethods/settings.py
+++
@@ -1,4 +0,0 @@
-import multiprocessing
-
-N_JOBS = -2  #multiprocessing.cpu_count()
-SAMPLE_SIZE = 100
--- TweetSentQuant/settings.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-N_JOBS = -2  #multiprocessing.cpu_count()
-CUDA_N_JOBS = 2
-ENSEMBLE_N_JOBS = -2
-
-SAMPLE_SIZE = 100
-
