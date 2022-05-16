84fbd9b3afc15602e020fe9ca57e3f110ca2b0d6
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2022-05-16 18:14:20 +0200

    preparing baselines

--- LeQua2022/run_baselines.sh
+++ LeQua2022/run_baselines.sh
@@ -21,7 +21,7 @@ set -x
 #PYTHONPATH=.:.. python3 prepickle_folder.py ./data/T2B.train_dev_BERT/T2B ./data/T2B.train_dev_BERT_pkl/T2B
 
 #PYTHONPATH=.:.. python3 baselines.py binary ./data/T1A.train_dev_pkl/T1A/public ./model/T1A_npy npy &
-#PYTHONPATH=.:.. python3 baselines.py multiclass ./data/T1B.train_dev_pkl/T1B/public ./model/T1B_npy npy &
+#PYTHONPATH=.:.. python3 baselines.py multiclass ./data/T1B.train_dev_pkl/T1B/public ./model/T1B_npy_mlpe npy
 #PYTHONPATH=.:.. python3 baselines.py binary ./data/T2A.train_dev/T2A/public ./model/T2A_tfidf_raw raw &
 #PYTHONPATH=.:.. python3 baselines.py multiclass ./data/T2B.train_dev/T2B/public ./model/T2B_tfidf_raw raw
 #PYTHONPATH=.:.. python3 baselines.py binary ./data/T2A.train_dev_BERT_pkl/T2A/public ./model/T2A_BERT_npy npy &
@@ -37,8 +37,6 @@ set -x
 #PYTHONPATH=.:.. python3 advanced_baselines.py multiclass ./data/T2B.train_dev_BERT_pkl/T2B/public ./model/T2B_BERT_npy npy 
 
 
-PYTHONPATH=.:.. python3 baseline_quanet.py binary ./data/T1A.train_dev_pkl/T1A/public ./data/T1A.test_pkl/public/test_samples quanet_model/T1A predictions/T1A/quanet_dev.csv predictions/T1A/quanet_test.csv
-PYTHONPATH=.:.. python3 baseline_quanet.py multiclass ./data/T1B.train_dev_pkl/T1B/public ./data/T1B.test_pkl/public/test_samples quanet_model/T1B predictions/T1B/quanet_dev.csv predictions/T1B/quanet_test.csv
+#PYTHONPATH=.:.. python3 baseline_quanet.py binary ./data/T1A.train_dev_pkl/T1A/public ./data/T1A.test_pkl/public/test_samples quanet_model/T1A predictions/T1A/quanet_dev.csv predictions/T1A/quanet_test.csv
 #PYTHONPATH=.:.. python3 baseline_quanet.py binary ./data/T2A.train_dev_BERT_pkl/T2A/public ./data/T2A.test_BERT_pkl/public/test_samples quanet_model/T2A predictions/T2A/quanet_dev.csv predictions/T2A/quanet_test.csv
-PYTHONPATH=.:.. python3 baseline_quanet.py multiclass ./data/T2B.train_dev_BERT_pkl/T2B/public ./data/T2B.test_BERT_pkl/public/test_samples quanet_model/T2B predictions/T2B/quanet_dev.csv predictions/T2B/quanet_test.csv
 
