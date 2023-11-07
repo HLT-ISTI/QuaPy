c0f9a50a141c4bd461a28cf659d33efc293ea0cf
Alex Moreo <alejandro.moreo@isti.cnr.it>
2023-11-07 17:28:32 +0100

    plotting simplex and 3d-histograms

--- distribution_matching/figures/simplex_density_plot2.py
+++ distribution_matching/figures/simplex_density_plot2.py
@@ -2,50 +2,37 @@ import ternary
 import math
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.neighbors import KernelDensity
 import plotly.figure_factory as ff
 
 from data import LabelledCollection
 
-scale = 10
+scale = 100
 
 
 # con ternary (una lib de matplotlib) salen bien pero no puedo crear contornos, o no se
 # con plotly salen los contornos bien, pero es un poco un jaleo porque utiliza el navegador...
 
-def plot_simplex_(ax, density, title='', fontsize=9, points=None):
+def plot_simplex_(ax, density, title='', fontsize=30, points=None):
 
     tax = ternary.TernaryAxesSubplot(ax=ax, scale=scale)
     tax.heatmapf(density, boundary=True, style="triangular", colorbar=False, cmap='viridis') #cmap='magma')
     tax.boundary(linewidth=1.0)
-    corner_fontsize = 5*fontsize//6
+    corner_fontsize = int(5*fontsize//6)
     tax.right_corner_label("$y=3$", fontsize=corner_fontsize)
     tax.top_corner_label("$y=2$", fontsize=corner_fontsize)
     tax.left_corner_label("$y=1$", fontsize=corner_fontsize)
     if title:
         tax.set_title(title, loc='center', y=-0.11, fontsize=fontsize)
     if points is not None:
-        tax.scatter(points*scale, marker='o', color='w', alpha=0.25, zorder=10)
+        tax.scatter(points*scale, marker='o', color='w', alpha=0.25, zorder=10, s=5*scale)
     tax.get_axes().axis('off')
     tax.clear_matplotlib_ticks()
 
     return tax
 
 
-def plot_simplex(ax, coord, kde_scores, title='', fontsize=11, points=None, savepath=None):
-
-    tax = ff.create_ternary_contour(coord.T, kde_scores, pole_labels=['y=1', 'y=2', 'y=3'],
-                                interp_mode='cartesian',
-                                ncontours=20,
-                                colorscale='Viridis',
-                                showscale=True,
-                                title=title)
-    if savepath is None:
-        tax.show()
-    else:
-        tax.write_image(savepath)
-    return tax
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 def plot_3class_problem(post_c1, post_c2, post_c3, post_test, alpha, bandwidth):
@@ -55,7 +42,7 @@ def plot_3class_problem(post_c1, post_c2, post_c3, post_test, alpha, bandwidth):
     post_test = np.flip(post_test, axis=1)
 
     size_=10
-    fig = ternary.plt.figure(figsize=(4*size_, 1*size_))
+    fig = ternary.plt.figure(figsize=(5*size_, 1*size_))
     fig.tight_layout()
     ax1 = fig.add_subplot(1, 4, 1)
     divider = make_axes_locatable(ax1)
@@ -101,17 +88,12 @@ def plot_3class_problem(post_c1, post_c2, post_c3, post_test, alpha, bandwidth):
             return total_density
         return m
 
-    title = '$p_{\mathbf{\\alpha}} = \sum_{i \in n} \\alpha_i p_i$'
+    title = '$\mathbf{p}_{\mathbf{\\alpha}} = \sum_{i \in n} \\alpha_i p_i$'
 
     plot_simplex_(ax4, mixture_(alpha, [kde1, kde2, kde3]), title=title, points=post_test)
-    #mixture(alpha, [kde1, kde2, kde3])
-
-    #post_test = np.concatenate([post_test, np.eye(3, dtype=float)])
-    #test_scores = sum(alphai*np.exp(kdei.score_samples(post_test)) for alphai, kdei in zip(alpha, [kde1,kde2,kde3]))
-    #plot_simplex(ax4, post_test, test_scores, title=title, points=post_test)
 
     #ternary.plt.show()
-    ternary.plt.savefig('./simplex.png')
+    ternary.plt.savefig('./simplex.pdf')
 
 
 import quapy as qp
@@ -119,33 +101,28 @@ import quapy as qp
 
 data = qp.datasets.fetch_twitter('wb', min_df=3, pickle=True, for_model_selection=False)
 
-X, y = data.training.Xy
+Xtr, ytr = data.training.Xy
+Xte, yte = data.test.sampling(150, *[0.5, 0.1, 0.4]).Xy
 
 cls = LogisticRegression(C=0.0001, random_state=0)
 
-Xtr, Xte, ytr, yte = train_test_split(X, y, train_size=0.7, stratify=y, random_state=0)
-
-cls.fit(Xtr, ytr)
-
-test = LabelledCollection(Xte, yte)
-test = test.sampling(100, *[0.2, 0.1, 0.7])
-
-Xte, yte = test.Xy
-
-post_c1 = cls.predict_proba(Xte[yte==0])
-post_c2 = cls.predict_proba(Xte[yte==1])
-post_c3 = cls.predict_proba(Xte[yte==2])
+draw_from_training = False
+if draw_from_training:
+    post_tr = cross_val_predict(cls, Xtr, ytr, n_jobs=-1, method='predict_proba')
+    post_c1 = post_tr[ytr==0]
+    post_c2 = post_tr[ytr==1]
+    post_c3 = post_tr[ytr==2]
+    cls.fit(Xtr, ytr)
+else:
+    cls.fit(Xtr, ytr)
+    post_te = cls.predict_proba(Xte)
+    post_c1 = post_te[yte == 0]
+    post_c2 = post_te[yte == 1]
+    post_c3 = post_te[yte == 2]
 
 post_test = cls.predict_proba(Xte)
-print(post_test)
+
 alpha = qp.functional.prevalence_from_labels(yte, classes=[0, 1, 2])
-
-#post_c1 = np.random.dirichlet([10,3,1], 30)
-#post_c2 = np.random.dirichlet([1,11,6], 30)
-#post_c3 = np.random.dirichlet([1,5,20], 30)
-#post_test = np.random.dirichlet([5,1,6], 100)
-#alpha = [0.5, 0.3, 0.2]
-
 
 print(f'test alpha {alpha}')
 plot_3class_problem(post_c1, post_c2, post_c3, post_test, alpha, bandwidth=0.1)
