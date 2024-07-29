6b053bc96685a329c5cd220ad2508738c69d5e68
pglez82 <pglez82@gmail.com>
2024-07-29 18:02:36 +0200

    new cd diagrams

--- distribution_matching/figures/CD_diagrams.py
+++ distribution_matching/figures/CD_diagrams.py
@@ -1,56 +1,81 @@
 import pandas as pd
 from critdd import Diagram
-from distribution_matching.commons import BIN_METHODS
+from distribution_matching.commons import BIN_METHODS, METHODS
 import quapy as qp
-
-error = 'mrae'
-
-datasets = qp.datasets.UCI_BINARY_DATASETS
-datasets_remove = ['iris.1', 'acute.a', 'acute.b'] 
-for d in datasets_remove:
-    if d in datasets:
-        datasets.remove(d)
-
-methods = BIN_METHODS
-methods.remove("MS")
-methods.remove("EMQ-BCTS")
-
-results = pd.DataFrame(columns=methods,index=datasets,dtype=float)
+import importlib
+from Orange.evaluation import scoring
+_ = importlib.reload( scoring )
 
 
-for method in methods:
-    for dataset in datasets:
-        df=pd.read_csv('binary/'+error+'/'+method+'_'+dataset+'.dataframe',sep=',')
-        results[method][dataset]=float(df[error].mean())
 
-print(results.columns)
-
-diagram = Diagram(
-    results.to_numpy(),
-    treatment_names = results.columns,
-    maximize_outcome = False
-)
-
-file_name = "CD_bin_"+error+".tex"
-
-diagram.to_file(
-    file_name,
-    alpha = .05,
-    adjustment = "holm",
-    reverse_x = True,
-    axis_options = {"title": error.upper()},
-)
-
-# Open the file in read mode
-with open(file_name, 'r') as file:
-    # Read the file content
-    content = file.read()
-
-modified_content = content.replace('EDy+', 'EDy')
-modified_content = modified_content.replace('PACC+', 'PACCy')
-modified_content = modified_content.replace('ACC+', 'ACC')
+def generate_cd(dataset_global, error):
+    if dataset_global=='binary':
+        datasets = qp.datasets.UCI_BINARY_DATASETS.copy()
+        datasets_remove = ['iris.1', 'acute.a', 'acute.b'] 
+        for d in datasets_remove:
+            if d in datasets:
+                datasets.remove(d)
+        methods = BIN_METHODS.copy()
+        methods.remove("EMQ-BCTS")
+        print(methods)
+    elif dataset_global=='ucimulti':
+        datasets = qp.datasets.UCI_MULTICLASS_DATASETS.copy()
+        methods = METHODS.copy()
+        methods.remove("EMQ-BCTS")
+    elif dataset_global=='tweet':
+        datasets = qp.datasets.TWITTER_SENTIMENT_DATASETS_TEST.copy()
+        methods = METHODS.copy()
+        methods.remove("EMQ-BCTS")
+    elif dataset_global=='multimix':
+        datasets = qp.datasets.UCI_MULTICLASS_DATASETS+qp.datasets.TWITTER_SENTIMENT_DATASETS_TEST+['lequa']
+        methods = METHODS.copy()
+        methods.remove("EMQ-BCTS")
 
 
-# Open the file in write mode and write the modified content
-with open(file_name, 'w') as file:
-    file.write(modified_content)
+
+    results = pd.DataFrame(columns=methods,index=datasets,dtype=float)
+
+
+    for method in methods:
+        for dataset in datasets:
+            df=pd.read_csv('results/'+dataset_global+'/'+error+'/'+method+'_'+dataset+'.dataframe',sep=',')
+            results[method][dataset]=float(df[error].mean())
+
+    results = results.rename(columns={'ACC+': 'ACC', 'PACC+':'PACC','EDy+':'EDy'})
+
+    diagram = Diagram(
+        results.to_numpy(),
+        treatment_names = results.columns,
+        maximize_outcome = False
+    )
+
+    file_name = "CD_"+dataset_global+"_"+error+".tex"
+
+    diagram.to_file(
+        file_name,
+        alpha = .05,
+        adjustment = "holm",
+        reverse_x = True,
+        axis_options = {"title": error.upper()},
+    )
+
+    print(results)
+
+    ranked_results = results.rank(axis=1, method='min', ascending=True)
+    # Calculate the average rank for each method
+    average_ranks = ranked_results.mean(axis=0)
+
+    names = average_ranks.index
+    avranks = average_ranks.to_numpy()
+    cd = scoring.compute_CD(avranks, len(ranked_results))
+    file_name = "CDOrange_"+dataset_global+"_"+error+".png"
+    scoring.graph_ranks(avranks, names, cd, width=6, textspace=1.5,filename=file_name)
+
+generate_cd('ucimulti','mae')
+generate_cd('ucimulti','mrae')
+generate_cd('binary','mae')
+generate_cd('binary','mrae')
+generate_cd('tweet','mae')
+generate_cd('tweet','mrae')
+generate_cd('multimix','mae')
+generate_cd('multimix','mrae')
