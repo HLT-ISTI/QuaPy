125d4bd75601876e67b9bddba81a4380ab604dba
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2026-07-17 17:39:33 +0200

    pin setuptools<82 in bayes extra
    setuptools 82 removed pkg_resources entirely, which httpstan still
    imports at runtime; CI's prior `pip install --upgrade setuptools`
    step grabs the latest, so an unpinned setuptools didn't help.

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={  # Optional
-       'bayes': ['jax', 'jaxlib', 'numpyro', 'pystan', 'setuptools'],
+       'bayes': ['jax', 'jaxlib', 'numpyro', 'pystan', 'setuptools<82'],
        'neural': ['torch'],
        'tests': ['certifi'],
        'docs' : ['sphinx-rtd-theme', 'myst-parser'],
